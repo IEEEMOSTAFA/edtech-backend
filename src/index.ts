aea651922e8dc47a3857b848e80bd47a117e5e7d
IEEEMOSTAFA <skmostafa8888@gmail.com>
2026-02-25 00:48:38 +0600

    corrected

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // import app from "./app";
 
 import app from "./app";
+// const app = express();
 
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
 // Export for Vercel serverless
 export default app;
